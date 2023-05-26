87e95618a9b5708c02ed58ba487c9f0a45f52878
perebonilado <reradiri@eminenttechnology.com>
2023-05-26 20:49:59 +0100

    logo update

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -8,7 +8,7 @@ const Logo: FC = () => {
   return (
     <div className="relative h-8 w-36 cursor-pointer" onClick={()=>router.push('/')}>
       <Image
-        src={"/logo.png"}
+        src={"https://res.cloudinary.com/dt0wfaxft/image/upload/v1685130491/logo_sulnht.png"}
         fill={true}
         alt="Kabukabu, ride hailing service"
         style={{ objectFit: "contain" }}
